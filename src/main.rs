0e8e3b94a8e7d43f1f2c29ee5ae3b6e020c4f6de
Zach W <zwells@email.sc.edu>
2023-02-25 19:26:02 -0500

    Completed section 2, onto section 3

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("P3\n {} {}\n255", image_width, image_height);
 
     for j in (0..(image_height - 1)).rev() {
+        eprintln!("Scanlines remaining: {}", j);
         for i in 0..image_width {
             let r = (i as f64) / ((image_width - 1) as f64);
             let g = (j as f64) / ((image_height - 1) as f64);
