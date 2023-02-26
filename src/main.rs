aff23d13d1697bd7cec5cf5851b0756f8324d8db
Zach W <zwells@email.sc.edu>
2023-02-25 19:15:02 -0500

    got through 2.2, creating an image file

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // image
+    let image_width = 256;
+    let image_height = 256;
+
+    // render
+    println!("P3\n {} {}\n255", image_width, image_height);
+
+    for j in (0..(image_height - 1)).rev() {
+        for i in 0..image_width {
+            let r = (i as f64) / ((image_width - 1) as f64);
+            let g = (j as f64) / ((image_height - 1) as f64);
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
